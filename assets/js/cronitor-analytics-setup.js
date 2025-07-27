34ab26b855cbff00695d770e4c973127c5eac01e
jpsaha <jpsaha@users.noreply.github.com>
2025-07-27 14:19:09 +0000

    Deploying to gh-pages from @ jpsaha/home@fc96288688c92ea31cab1eda849dda5d3ef30a91 🚀

--- assets/js/cronitor-analytics-setup.js
+++ assets/js/cronitor-analytics-setup.js
@@ -3,4 +3,4 @@ window.cronitor =
   function () {
     (window.cronitor.q = window.cronitor.q || []).push(arguments);
   };
-cronitor("config", { clientKey: "" });
+cronitor("config", { clientKey: "96b9516e83a52ad24850b802e6c61141" });
